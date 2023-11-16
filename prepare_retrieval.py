65b491861243fe3aab58b772aadaca74bb161ff4
Dodero10 <Dat.TC215346@sis.hust.edu.vn>
2023-11-17 05:01:46 +0700

    update model and UI

--- prepare_retrieval.py
+++ prepare_retrieval.py
@@ -1,43 +1,35 @@
 import shutil
-
 import rank_bm25
 import os
-
 from rank_bm25 import BM25Okapi
 
-
 def get_bm25_scores(question):
-
+    # Get all file names and contents from the data folder
     file_names = []
     corpus = []
     for file_name in os.listdir('./data'):
-        with open(f'./data/{file_name}', 'r',encoding='cp437') as f:
+        with open(f'./data/{file_name}', 'r', encoding='cp437') as f:
             doc = f.readlines()
 
         file_names.append(" ".join(file_name.split("-")))
         corpus.append(" ".join(doc))
 
-    # import matplotlib.pyplot as plt
-    # plt.hist([len(doc.split()) for doc in corpus], bins=128, range=(0, 5000))
-    # plt.show()
-
+    # Tokenize the titles
     titles = file_names
-
     words = [
         [word for word in doc.split()]
         for doc in titles
     ]
+
+    # Calculate BM25 scores
     bm25 = BM25Okapi(words)
     indexs = list(range(len(words)))
-
     tokenized_query = question.split()
-
     a = bm25.get_top_n(tokenized_query, indexs, n=5)
-    folder_path = 'retrieval'
 
-    # Kiểm tra nếu thư mục tồn tại
+    # Create or clear the retrieval folder
+    folder_path = 'retrieval'
     if os.path.exists(folder_path):
-        # Xóa tất cả các file trong thư mục
         for filename in os.listdir(folder_path):
             file_path = os.path.join(folder_path, filename)
             try:
@@ -48,12 +40,10 @@ def get_bm25_scores(question):
             except Exception as e:
                 print('Failed to delete %s. Reason: %s' % (file_path, e))
     else:
-        # Nếu thư mục không tồn tại, tạo thư mục mới
         os.makedirs(folder_path)
 
-    # Lưu file mới
+    # Save the top 5 files to the retrieval folder
     for i in range(len(a)):
         file_path = os.path.join(folder_path, titles[a[i]])
-
         with open(file_path, 'w', encoding='utf-8') as file:
             file.write(corpus[a[i]])
